03989b16b5fcd53b31b43033087b6e23de9b16d2
VictorEyeEagle <victor100bol@hotmail.com>
2024-11-30 19:40:24 -0300

    .

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,34 @@
+"""
+URL configuration for pweb project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [  
-    path('' ,views.index, name='index'),
+urlpatterns = [
+    path('', views.index, name="index"),
     path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='entre_em_contato'), 
+    path('contato/', views.contato, name='contato'),
+    path('item/<int:id>/', views.exibir_item, name='item'),
+    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    path('diasemana/<int:dia>/', views.diasemana, name='diasemana'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('produtos/form', views.form_produto, name='form_produto'),
+    path('produtos/detalhes/<int:id>/', views.detalhes_produtos, name='detalhes_produto'),
+    path('produtos/editar/<int:id>/', views.editar_produto, name='editar_produto'),
+    path('produtos/excluir/<int:id>/', views.excluir_produto, name='excluir_produto'),
+]   
 
-    # captura do item id
-    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
-
-
-]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,77 @@
 from django.shortcuts import render
-import datetime
+from .forms import ContatoForm, ProdutoForm
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request,'index.html')
+
 def sobre(request):
     return render(request,'sobre.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    return render(request,'contato.html')
 
-def exibir_item(request,id):
-    return render(request,'exibir_item.html',{'id':id})
+def exibir_item(request, id):
+    return render(request,"item.html",{'id':id})
 
+def perfil(request,usuario):
+    return render(request,"perfil.html",{'perfil':usuario})
+
+def diasemana(request,dia):
+    dia_semana = ""
+    if dia == 1:
+        dia_semana = 'Domingo'
+    elif dia == 2:
+        dia_semana = 'Segunda-Feira'
+    elif dia == 3:     
+        dia_semana = 'Terça-Feira'
+    elif dia == 4:
+        dia_semana = 'Quarta-Feira'
+    elif dia == 5:  
+        dia_semana = 'Quinta-Feira'
+    elif dia == 6:  
+        dia_semana = 'Sexta-Feira'
+    elif dia == 7:  
+        dia_semana = 'Sábado'
+    else:  
+        dia_semana = 'Inválido' 
+    
+    return render(request, "diasemana.html", {'num':dia, 'dia':dia_semana})
+    
+def produtos(request):
+    contexto = {
+        'lista': [
+            {'id':1, 'nome':'Notebook', 'preco':'2.500,00'},
+            {'id':2, 'nome':'Monitor', 'preco':'500,00'},
+            {'id':3, 'nome':'Teclado', 'preco':'80,00'},
+        ],
+    }
+    return render(request, 'produto/lista.html', contexto)
+
+def contato(request):
+    form = ContatoForm()
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'contato.html', contexto)
+
+def form_produto(request):
+    form = ProdutoForm()
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'produto/formulario.html', contexto)
+
+def detalhes_produtos(request, id):
+    return render(request, 'produto/detalhes.html', {'item':id})
+
+def editar_produto(request, id):
+    form = ProdutoForm()
+    contexto = {
+        'form': form,
+        'item': id,
+    }
+    return render(request, 'produto/formulario.html', contexto)
+
+def excluir_produto(request, id):
+    return render(request, 'produto/excluir.html', {'item':id})
